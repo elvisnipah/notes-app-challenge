7dac0399074ec758955fce89f733c79124163095
Elvis <elvisnipah@gmail.com>
2022-12-24 14:31:53 +0000

    tidy up routes

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,41 +1,27 @@
 const express = require("express");
-
-const Note = require("../models/noteModel");
-
 const router = express.Router();
 
+const {
+  createNote,
+  getNotes,
+  getNote,
+  deleteNote,
+  updateNote,
+} = require("../controllers/noteController");
+
 //GET all notes
-router.get("/", (req, res) => {
-  res.json({
-    msg: "GET all notes",
-  });
-});
+router.get("/", getNotes);
 
 //GET a single note
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET a single note" });
-});
+router.get("/:id", getNote);
 
 //POST a new note
-router.post("/", async (req, res) => {
-  const { title, body } = req.body;
-
-  try {
-    const note = await Note.create({ title, body });
-    res.status(200).json(note);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createNote);
 
 //DELETE a note
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "DELETE a note" });
-});
+router.delete("/:id", deleteNote);
 
 //UPDATE a note
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "UPDATE a note" });
-});
+router.patch("/:id", updateNote);
 
 module.exports = router;
