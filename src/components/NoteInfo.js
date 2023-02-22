0bbc716d6c1c745b2eb8529f864129e06355b600
Elvis <elvisnipah@gmail.com>
2023-02-22 08:11:30 +0000

    visual changes to note display

--- src/components/NoteInfo.js
+++ src/components/NoteInfo.js
@@ -4,43 +4,49 @@ import React from "react";
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 function NoteInfo({ note }) {
-  async function handleClick() {
-    const response = await fetch("/api/notes/" + note._id, {
-      method: "DELETE",
-    });
+    async function handleClick() {
+        const response = await fetch("/api/notes/" + note._id, {
+            method: "DELETE",
+        });
 
-    if (response.ok) {
-      window.location.reload(false);
+        if (response.ok) {
+            window.location.reload(false);
+        }
     }
-  }
 
-  return (
-    <div className="m-3 p-4 bg-slate-200 rounded-lg max-w-lg justify-between flex flex-col gap-2">
-      <a
-        href={"/" + note._id}
-        className="font-bold text-xl text-blue-700 hover:underline"
-      >
-        {note.title.slice(0, 25)}
-      </a>
-      <p className="">{note.body.slice(0, 25)}...</p>
-      <p className="text-sm">
-        Created{" "}
-        {formatDistanceToNow(new Date(note.createdAt), { addSuffix: true })}
-      </p>
-      <p className="text-sm">
-        Last Updated{" "}
-        {formatDistanceToNow(new Date(note.updatedAt), { addSuffix: true })}
-      </p>
-      <div className="flex">
-        <button
-          onClick={handleClick}
-          className="px-3 py-2 font-bold rounded-lg bg-red-600 text-white"
-        >
-          Delete
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className="m-3 p-4 bg-slate-200 rounded-lg max-w-lg justify-between flex flex-col gap-2">
+            <a
+                href={"/" + note._id}
+                className="font-bold text-xl text-blue-700 hover:underline"
+            >
+                {note.title.slice(0, 25)}
+            </a>
+            <p className="">
+                {note.body.slice(0, 25)} {note.body.length > 25 ? "[...]" : ""}
+            </p>
+            <p className="text-sm text-slate-500">
+                Created{" "}
+                {formatDistanceToNow(new Date(note.createdAt), {
+                    addSuffix: true,
+                })}
+            </p>
+            <p className="text-sm text-slate-500">
+                Last updated{" "}
+                {formatDistanceToNow(new Date(note.updatedAt), {
+                    addSuffix: true,
+                })}
+            </p>
+            <div className="flex">
+                <button
+                    onClick={handleClick}
+                    className="px-3 py-2 font-bold rounded-lg bg-red-600 text-white"
+                >
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default NoteInfo;
--- src/pages/UpdateNote.js
+++ src/pages/UpdateNote.js
@@ -2,116 +2,116 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 function UpdateNote() {
-  const { id } = useParams();
+    const { id } = useParams();
 
-  useEffect(() => {
-    const fetchNote = async () => {
-      const response = await fetch("/api/notes/" + id);
-      const json = await response.json();
+    useEffect(() => {
+        const fetchNote = async () => {
+            const response = await fetch("/api/notes/" + id);
+            const json = await response.json();
 
-      if (response.ok) {
-        setFormData({
-          title: json.title,
-          body: json.body,
-        });
-      }
-    };
+            if (response.ok) {
+                setFormData({
+                    title: json.title,
+                    body: json.body,
+                });
+            }
+        };
 
-    fetchNote();
-  }, [id]);
+        fetchNote();
+    }, [id]);
 
-  // create state for the note form using the id
-  const [formData, setFormData] = useState({
-    title: "",
-    body: "",
-  });
-
-  const [error, setError] = useState(null);
-
-  //store new note values in the formData state
-  function handleChange(event) {
-    setFormData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [event.target.name]: event.target.value,
-      };
-    });
-  }
-
-  const [isError, setIsError] = useState(false);
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const response = await fetch("/api/notes/" + id, {
-      method: "PATCH",
-      body: JSON.stringify(formData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const json = await response.json();
-
-    if (!response.ok) {
-      setError(json.error);
-      setIsError(true);
-    }
-
-    if (response.ok) {
-      setFormData({
+    // create state for the note form using the id
+    const [formData, setFormData] = useState({
         title: "",
         body: "",
-      });
-      setError(null);
-      setIsError(false);
-      console.log("note updated");
-      window.location.href = "/";
+    });
+
+    const [error, setError] = useState(null);
+
+    //store new note values in the formData state
+    function handleChange(event) {
+        setFormData((prevFormData) => {
+            return {
+                ...prevFormData,
+                [event.target.name]: event.target.value,
+            };
+        });
     }
-  }
 
-  const ErrorMessage = () => {
+    const [isError, setIsError] = useState(false);
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        const response = await fetch("/api/notes/" + id, {
+            method: "PATCH",
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setError(json.error);
+            setIsError(true);
+        }
+
+        if (response.ok) {
+            setFormData({
+                title: "",
+                body: "",
+            });
+            setError(null);
+            setIsError(false);
+            //   console.log("note updated");
+            window.location.href = "/";
+        }
+    }
+
+    const ErrorMessage = () => {
+        return (
+            <div className="self-center border-2 border-red-600 text-red-600 p-2 text-center">
+                {error}
+            </div>
+        );
+    };
+
     return (
-      <div className="self-center border-2 border-red-600 text-red-600 p-2 text-center">
-        {error}
-      </div>
+        <form
+            className="flex flex-col justify-center gap-5 p-5"
+            onSubmit={handleSubmit}
+        >
+            <input
+                type="text"
+                placeholder="Title..."
+                onChange={handleChange}
+                name="title"
+                value={formData.title}
+                className="p-4 font-bold"
+            />
+
+            <textarea
+                name="body"
+                id="body"
+                cols="30"
+                rows="10"
+                value={formData.body}
+                placeholder="Body..."
+                onChange={handleChange}
+                className="p-4 border-solid border-2 border-sky-300 rounded-lg"
+            ></textarea>
+
+            <input
+                type="submit"
+                value="Save Note"
+                className="bg-green-400 w-32 p-3 rounded-xl text-white font-bold self-center hover:border-emerald-900 hover:bg-green-700 hover:cursor-pointer"
+            />
+
+            {isError && <ErrorMessage />}
+        </form>
     );
-  };
-
-  return (
-    <form
-      className="flex flex-col justify-center gap-5 p-5"
-      onSubmit={handleSubmit}
-    >
-      <input
-        type="text"
-        placeholder="Title..."
-        onChange={handleChange}
-        name="title"
-        value={formData.title}
-        className="p-4 font-bold"
-      />
-
-      <textarea
-        name="body"
-        id="body"
-        cols="30"
-        rows="10"
-        value={formData.body}
-        placeholder="Body..."
-        onChange={handleChange}
-        className="p-4 border-solid border-2 border-sky-300 rounded-lg"
-      ></textarea>
-
-      <input
-        type="submit"
-        value="Save Note"
-        className="bg-green-400 w-32 p-3 rounded-xl text-white font-bold self-center hover:border-emerald-900 hover:bg-green-700 hover:cursor-pointer"
-      />
-
-      {isError && <ErrorMessage />}
-    </form>
-  );
 }
 
 export default UpdateNote;
